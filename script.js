c3a5cf8d3c096b93482ad0cd7529abc920aebe8a
JK <saikitjk@gmail.com>
2020-07-17 23:27:10 -0700

    created ajax get method to retrieve weather data

--- script.js
+++ script.js
@@ -6,28 +6,51 @@ $(document).ready(function(){
 
         var city = $(this).attr("city-name");
         //console.log("the city is "+city);
-        var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=7fff9c3c870a804f5643f8216e943977";
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&apikey=7fff9c3c870a804f5643f8216e943977";
         //console.log("this is " + queryURL);
+        //UV Query
+        
         // Creates AJAX call 
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(response) {
 
+            //get UV Index
+
             //create button to hold cities
             var cityDiv = $("<div class='city'>");
             // Retrieves the city Data
             var temp = response.main.temp;
+            var humidity = response.main.humidity;
+            var windSpeed = response.wind.speed;
+            //UV Query
+            var lat = response.coord.lon;
+            var lon = response.coord.lat;
+            console.log("the lon and lat are "+lon+ "&"+lat);
+            var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?"+lat+"&"+lon+"&apikey=7fff9c3c870a804f5643f8216e943977";
+            //console.log("this is " + uvQueryURL);
+            $.ajax({
+                url: uvQueryURL,
+                method: "GET"
+                }).then(function(response) {
+                     //get UV Index
+                    var uvIndex = response.value;
+                });
+
+            //get UV Index
+
+
+            //get UV Index
+
+
+            //create div to store the retrieved data
             var displayTemp = $("<div>").text("Temp: " + temp);
             console.log(displayTemp, temp);
             // Displays the temp
             cityDiv.append(displayTemp);
     
-
-
-            $("#city-content").prepend(cityDiv);
-
-
+            $("#city-content").append(cityDiv);
 
             });
 
