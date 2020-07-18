2bbd76cbf37bfef97a173f67b0dd7defcaf5c772
JK <saikitjk@gmail.com>
2020-07-17 19:55:34 -0700

    rearranged how the city button get append into html

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
-var cityArry = ["Seattle"];
 
-/*function displayCity(){
+$(document).ready(function(){
+    var cityArry = ["Seattle","NY","HK"];
 
-    var city = $(this).attr("city-name");
-    var queryURL = "" + city + "&apikey=";
+    /*function displayCity(){
 
-    // Creates AJAX call for the specific movie button being clicked
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
+        var city = $(this).attr("city-name");
+        var queryURL = "" + city + "&apikey=";
 
-      //create button to hold cities
-      var cityDiv = $("<div class='city'>");
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
 
-      // Retrieves the city Data
-      
-      // Displays the rating
-      cityDiv.append();
+        //create button to hold cities
+        var cityDiv = $("<div class='city'>");
 
-
-      $("#city-content").prepend(cityDiv)
-
+        // Retrieves the city Data
+        
+        // Displays the rating
+        cityDiv.append();
 
 
+        $("#city-content").prepend(cityDiv)
+
+
+
+        });
+
+    }*/
+
+    function generateButton(){
+        $("#city-buttons").empty();
+        
+        //console.log(cityArry, cityArry.length);
+        // Loops through the array of movies
+        for (var i = 0; i < cityArry.length; i++) {
+        var buttonDiv = $("<div>");
+        var cityButton = $("<button>");
+        cityButton.addClass("city");
+        cityButton.attr("data-name", cityArry[i]);
+        cityButton.text(cityArry[i]);
+        buttonDiv.append(cityButton);
+        $("#city-buttons").prepend(buttonDiv);
+        }
+    }
+
+    //on click the search button
+    $("#city-submit").on("click", function(event) {
+        event.preventDefault();
+        //console.log("search button clicked");
+        var city = $("#city-input").val().trim();
+        if(city !== ""){
+            cityArry.push(city);
+            console.log(cityArry);
+            generateButton();
+        }
+        else{
+            return;
+        }
     });
 
-}*/
+    generateButton();
 
-function generateButton(){
-    $("#city-buttons").empty();
-    console.log("hi");
-    // Loops through the array of movies
-    for (var i = 0; i < cityArry.length; i++) {
-      var cityButton = $("<button>");
-      cityButton.addClass("city");
-      cityButton.attr("data-name", cityArry[i]);
-      cityButton.text(cityArry[i]);
-      $("#city-buttons").append(cityButton);
-    }
-  }
-
-  //on click the search button
-  $("#city-submit").on("click", function(event) {
-    event.preventDefault();
-    console.log("search button clicked");
-    var city = $("#city-input").val().trim();
-    cityArry.push(city);
-    generateButtons();
-  });
-
+});
 
 
 
