67fa1cb65dc2dbc18359ab66f898d40953873405
JK <saikitjk@gmail.com>
2020-07-21 16:53:41 -0700

    added geolocation api

--- script.js
+++ script.js
@@ -182,7 +182,9 @@ $(document).ready(function(){
         var cityButton = $("<button>");
         cityButton.addClass("city");
         cityButton.css("margin-bottom","10px");
-        cityButton.css("background-color","white");
+        cityButton.css("background-color","#008CBA");
+        cityButton.css("color","white");
+        cityButton.css("border-radius", "4px");
         cityButton.attr("city-name", cityArry[i]);
         cityButton.text(cityArry[i]);
         buttonDiv.append(cityButton);
@@ -275,6 +277,43 @@ $(document).ready(function(){
         $("#displayUnitSelection").append(unitHTML);
         
    });
+
+   function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if(!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+  
+  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+
+
+
+
 
     generateButton();
 
